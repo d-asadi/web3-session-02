b1e2455869aa3f6ba2bcce3d2275d440924ced9d
delnia <del.asadi77@gmail.com>
2020-02-28 03:33:22 -0800

    session-2

---
+++ app.js
@@ -0,0 +1,45 @@
+var a = 5;
+var b = 10;
+var c = "5";
+var d = false;
+var arr = [1, 2, 3, true, 4, false];
+
+
+d1 = (!(b<5) && b >=(a*2));
+console.log(d1);
+
+
+d2 = (!(a == c) || a !==c);
+console.log(d2);
+
+
+d3 = (d == false && b-7 > a && d);
+console.log(d3);
+
+
+d4 = (a && b && c);
+console.log(d4);
+
+
+d5 = (!a || !d == true || !d);
+console.log(d5);
+
+
+d6 = (a >= (b-5) && 0);
+console.log(d6);
+
+
+d7 = (arr[10] || !(!d == false));
+console.log(d7);
+
+
+d8 = (arr.lenght > 0 && (b - a) === c);
+console.log(d8);
+
+
+d9 = (arr || false);
+console.log(d9);
+
+
+dd9 = (arr[0] && arr[0] == 1);
+console.log(dd9);
